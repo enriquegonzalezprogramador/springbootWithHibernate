ba8acab6341d9d1d3c03bfbcb5b4a66c652ee5da
JuniorGonzalez <twd00027@gmail.com>
2022-04-17 14:52:49 -0300

    primer commit

---
+++ src/main/java/com/juniorgonzalez/spring/SpringbootWithHibernateApplication.java
@@ -0,0 +1,13 @@
+package com.juniorgonzalez.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWithHibernateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootWithHibernateApplication.class, args);
+    }
+
+}
